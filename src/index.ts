ca279b1d8e5a91f4533a8b9a5e284430225907bc
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2024-06-20 22:21:46 +0100

    remove all dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 // Dependencies
-import got, { Got } from "got"
 import { IStatus } from "./interfaces/IStatus.js"
 import { IKeyDetails } from "./interfaces/IKeyDetails.js"
 import { IKeyCreate, IKeyCreatePayload } from "./interfaces/IKeyCreate.js"
 import { IDeleteKey } from "./interfaces/IDeleteKey.js"
-import { IUser, IUserGet } from "./interfaces/IUser.js"
+import { IUserGet } from "./interfaces/IUser.js"
 import { IUsers } from "./interfaces/IUsers.js"
 import { IResetHWID, IResetHWIDPayload } from "./interfaces/IResetHWID.js"
 import { ILinkDiscord, ILinkDiscordPayload } from "./interfaces/ILinkDiscord.js"
@@ -15,7 +14,7 @@ import { IScriptCreate, IScriptCreatePayload } from "./interfaces/IScriptCreate.
 export class LuarmorClient {
     // Vars
     private apiKey!: string
-    private httpClient!: Got
+    private baseURL = "https://api.luarmor.net/"
 
     // Constructor
     constructor(apiKey: string) {
@@ -25,89 +24,105 @@ export class LuarmorClient {
     // Sets the API Key and resets the HttpClient
     setAPIKey(apiKey: string) {
         this.apiKey = apiKey
-        this.httpClient = got.extend({
-            prefixUrl: "https://api.luarmor.net/",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": apiKey
-            }
-        })
     }
 
+    private headers() {
+        return {
+            "Content-Type": "application/json",
+            "Authorization": this.apiKey
+        }
+    }
+
+    async get<T>(url: string): Promise<T> {
+        return fetch(this.baseURL + url, {
+            method: "GET",
+            headers: this.headers(),
+        }).then(res => res.json())
+    }
+
+    async post<T>(url: string, json?: Object): Promise<T> {
+        return fetch(this.baseURL + url, {
+            method: "POST",
+            headers: this.headers(),
+            body: JSON.stringify(json)
+        }).then(res => res.json())
+    }
+
+    async patch<T>(url: string, json?: Object): Promise<T> {
+        return fetch(this.baseURL + url, {
+            method: "PATCH",
+            headers: this.headers(),
+            body: JSON.stringify(json)
+        }).then(res => res.json())
+    }
+
+    async put<T>(url: string, json?: Object): Promise<T> {
+        return fetch(this.baseURL + url, {
+            method: "PUT",
+            headers: this.headers(),
+            body: JSON.stringify(json)
+        }).then(res => res.json())
+    }
+
+    async delete<T>(url: string): Promise<T> {
+        return fetch(this.baseURL + url, {
+            method: "DELETE",
+            headers: this.headers(),
+        }).then(res => res.json())
+    }
+    
     // Grab API status
     async status() {
-        return (await this.httpClient.get("status").json()) satisfies IStatus
+        return this.get<IStatus>("status")
     }
 
     // Gets the details of a API key
     async keyDetails() {
-        return (await this.httpClient.get(`v3/keys/${this.apiKey}/details`).json()) satisfies IKeyDetails 
+        return this.get<IKeyDetails>(`v3/keys/${this.apiKey}/details`)
     }
 
     // Gets the stats of a key
     async keyStats(noUsers = false) {
-        return (await this.httpClient.get(`v3/keys/${this.apiKey}/stats`, {
-            searchParams: {
-                noUsers
-            }
-        }).json()) satisfies IKeyDetails 
+        return this.get<IKeyDetails>(`v3/keys/${this.apiKey}/stats?noUsers=${noUsers}`)
     }
 
     // Create a new key/user
     async createKey(project_id: string, payload: IKeyCreatePayload) {
-        return (await this.httpClient.post(`v3/projects/${project_id}/users`, {
-            json: payload
-        }).json()) satisfies IKeyCreate
+        return this.post<IKeyCreate>(`v3/projects/${project_id}/users`, payload)
     }
 
     // Updating an existing user
     async updateUser(project_id: string, payload: IKeyUpdatePayload) {
-        return (await this.httpClient.patch(`v3/projects/${project_id}/users`, {
-            json: payload
-        }).json()) satisfies IKeyUpdate
+        return this.patch<IKeyUpdate>(`v3/projects/${project_id}/users`, payload)
     }
 
     // Delete the key
     async deleteKey(project_id: string, userKey: string) {
-        return (await this.httpClient.delete(`v3/projects/${project_id}/users`, {
-            searchParams: {
-                user_key: userKey
-            }
-        }).json()) satisfies IDeleteKey
+        return this.delete<IDeleteKey>(`v3/projects/${project_id}/users?user_key=${userKey}`)
     }
 
     // Getting users
     async getUsers(project_id: string, payload: IUserGet = {}) {
-        return (await this.httpClient.get(`v3/projects/${project_id}/users`, {
-            searchParams: <any>payload
-        }).json()) satisfies IUsers
+        return this.get<IUsers>(`v3/projects/${project_id}/users?${new URLSearchParams(payload as any).toString()}`)
     }
 
     // Reset HWID of key
     async resetHWID(project_id: string, payload: IResetHWIDPayload) {
-        return (await this.httpClient.post(`v3/projects/${project_id}/users/resethwid`, {
-            json: payload
-        }).json()) satisfies IResetHWID
+        return this.post<IResetHWID>(`v3/projects/${project_id}/users/resethwid`, payload)
     }
 
     // Link Discord ID to a key
     async linkDiscord(project_id: string, payload: ILinkDiscordPayload) {
-        return (await this.httpClient.post(`v3/projects/${project_id}/users/linkdiscord`, {
-            json: payload
-        }).json()) satisfies ILinkDiscord
+        return this.post<ILinkDiscord>(`v3/projects/${project_id}/users/linkdiscord`, payload)
     }
 
     // Create a new script
     async createScript(project_id: string, payload: IScriptCreatePayload) {
-        return (await this.httpClient.post(`v3/projects/${project_id}/users/scripts`, {
-            json: payload
-        }).json()) satisfies IScriptCreate
+        return this.post<IScriptCreate>(`v3/projects/${project_id}/users/scripts`, payload)
     }
 
     // Update an existing script
     async updateScript(project_id: string, script_id: string, payload: IScriptCreatePayload) {
-        return (await this.httpClient.put(`v3/projects/${project_id}/users/scripts/${script_id}`, {
-            json: payload
-        }).json()) satisfies IScriptCreate
+        return this.put<IScriptCreate>(`v3/projects/${project_id}/users/scripts/${script_id}`, payload)
     }
 }
